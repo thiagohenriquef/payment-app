30249c55ef5f25c0790d8221eb792616fd5dcdd2
Thiago Henrique Ferreira <thiago.ferreira10@hotmail.com>
2018-11-25 00:29:29 -0200

    updating libs

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
+import { HashRouter } from 'react-router-dom'
 
 import Header from '../common/template/header'
 import Sidebar from '../common/template/sidebar'
 import Footer from '../common/template/footer'
 import Messages from '../common/msg/messages'
 
+import Routes from './routes'
+
 export default props => (
-  <div className='wrapper'>
-    <Header />
-    <Sidebar />
-    <div className='content-wrapper'>
-      {props.children}
+  <HashRouter>
+    <div className='wrapper'>
+      <Header />
+      <Sidebar />
+      <Routes />
+      <Footer />
+      <Messages />
     </div>
-    <Footer />
-    <Messages />
-  </div>
+  </HashRouter>
 )
--- src/main/reducers.js
+++ src/main/reducers.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux'
 import { reducer as formReducer } from 'redux-form'
 import { reducer as toastrReducer } from 'react-redux-toastr'
 
-import DashboardReducer from '@/Dashboard/dashboardReducer'
+import DashboardReducer from '../components/Dashboard/dashboardReducer'
 import tabReducer from '../common/tab/tabReducer'
 import billingCycleReducer from '../components/BillingCycle/billingCycleReducer'
 import authReducer from '../auth/authReducer'
